7ed5adae8593e1a04e1ebb56775f594cc849a0f8
bhanupgpt <98256435+bhanupgpt@users.noreply.github.com>
2023-07-15 12:56:17 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (76.15%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,20 +1,25 @@
 class MyStack {
 public:
-    queue <int> q1;
+    // queue <int> q1;
     queue <int> q2;
     MyStack() {
         
     }
     
     void push(int x) {
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
+        // q1.push(x);
+        // while(!q2.empty()){
+        //     q1.push(q2.front());
+        //     q2.pop();
+        // }
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        q2.push(x);
+        for(int i=0; i<q2.size()-1; i++) {
+            q2.push(q2.front());
             q2.pop();
-        }
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
         }
     }
     
