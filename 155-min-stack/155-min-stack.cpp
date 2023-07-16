fd207bd7cd7496cb6b851114235bea184634e6f3
bhanupgpt <98256435+bhanupgpt@users.noreply.github.com>
2023-07-16 22:12:22 +0530

    Time: 23 ms (74.72%), Space: 16.3 MB (28.20%) - LeetHub

---
+++ 155-min-stack/155-min-stack.cpp
@@ -0,0 +1,50 @@
+class MinStack {
+public:
+    stack <int> st1;
+    stack <int> st2;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st1.empty()){
+            st1.push(val);
+            st2.push(val);
+        }
+        else if(val > st2.top()){
+            st1.push(val);
+        }
+        else {
+            st1.push(val);
+            st2.push(val);
+        }
+    }
+    
+    void pop() {
+        int ans = st1.top();
+        if(st2.top() == st1.top()) {
+            st1.pop();
+            st2.pop();
+        }
+        else {
+            st1.pop();
+        }
+    }
+    
+    int top() {
+        return st1.top();
+    }
+    
+    int getMin() {
+        return st2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
