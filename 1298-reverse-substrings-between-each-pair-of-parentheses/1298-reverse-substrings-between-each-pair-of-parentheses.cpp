9e738404b60c8cd9418d6a8c075b92726eea3598
bhanupgpt <98256435+bhanupgpt@users.noreply.github.com>
2024-07-11 22:07:26 +0530

    Time: 3 ms (49.64%), Space: 7.9 MB (49.11%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        stack<int> openParenthesesIndices;
+        stack<int> st;
         string result;
-        for (char currentChar : s) {
-            if (currentChar == '(') {
-                // Store the current length as the start index for future
-                // reversal
-                openParenthesesIndices.push(result.length());
-            } else if (currentChar == ')') {
-                int start = openParenthesesIndices.top();
-                openParenthesesIndices.pop();
-                // Reverse the substring between the matching parentheses
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(') {
+                st.push(result.size());
+            }
+            else if(s[i] == ')') {
+                int start = st.top();
+                st.pop();
+
                 reverse(result.begin() + start, result.end());
-            } else {
-                // Append non-parenthesis characters to the processed string
-                result += currentChar;
+
+            }
+
+            else {
+                result += s[i];
             }
         }
+
         return result;
     }
 };
