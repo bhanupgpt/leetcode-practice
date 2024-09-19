ca0c7c7b210d1ca9afb34d9e8414229a65ad5cc6
bhanupgpt <bhanu@kgpian.iitkgp.ac.in>
2024-09-19 17:25:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        string ans="";
+        stringstream ss(str);
+        string word;
+        while(getline(ss,word,'.')){
+            ans=word+"."+ans;
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout << obj.reverseWords(s) << endl;
+    }
+}
+// } Driver Code Ends
